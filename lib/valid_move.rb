80a098979615aa00ad2c9076339b899c865f3dfa
boyloe <boyloe@gmail.com>
2020-07-16 09:41:41 -0600

    Submitting

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
-# code your #valid_move? method here
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+#Check if the position on board is already occupied and that the input is a valid location on the board.
+def valid_move?(board, index)
+    def position_taken?(array, index)
+      if array[index] == " " || array[index] == "" || array[index] == nil
+         false
+      else
+         true
+      end
+    end
+  
+    def on_board?(num)
+      if num.between?(0, 8) == true
+         true
+      else
+         false
+      end
+    end
+  
+    if (position_taken?(board, index)) == false && (on_board?(index) == true)
+       true
+    else
+       false
+    end 
+end
